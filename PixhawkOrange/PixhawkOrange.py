13eb821465e24ab29068c92007065c01aa6749cc
enfyna <legendraslan@gmail.com>
2023-10-03 12:27:29 +0300

    Add i parameter to hareket_et() Adjust code

--- PixhawkOrange/PixhawkOrange.py
+++ PixhawkOrange/PixhawkOrange.py
@@ -1,7 +1,5 @@
 from pymavlink import mavutil
-import sys
-import time
-import math
+from time import sleep
 
 class Dolunay():
 
@@ -9,73 +7,78 @@ class Dolunay():
     ERROR_OUT_OF_LOOP = 1
 
     def __init__(self, baglanti_modu):
-        def baglan():
-            platform = sys.platform.lower()
-            print("İsletim Sistemi : " + str(platform))
-            i = 0
-            if baglanti_modu == "USB":
-                while True:
-                    print(i)
-                    if platform == "linux":
-                        port = '/dev/ttyACM' + str(i)
-                    elif platform == "win32":
-                        port = 'COM' + str(i)
-                    else:
-                        raise Exception("Bilinmeyen isletim sistemi ?")
-                    try:
-                        master = mavutil.mavlink_connection(port)
-                        master.wait_heartbeat(blocking=False)
-                        print("Arac ile baglanti kuruldu.")
-                        break
-                    except:
-                        i += 1
-                        if i > 20:
-                            raise Exception(
-                                "Arac ile baglanti kurulamadi.Port bilgisinde hata var yada kablo duzgun takilmamis olabilir.")
-                        continue
-            elif baglanti_modu == "SITL":
-                port = 'udp:127.0.0.1:14550'
+        if baglanti_modu == "USB":
+
+            from sys import platform
+
+            OS = platform.lower()
+            print(f"İsletim Sistemi : {OS}")
+
+            if OS == "linux":
+                port = '/dev/ttyACM'
+            elif OS == "win32":
+                port = 'COM'
+            else:
+                raise Exception("Bilinmeyen isletim sistemi ?")
+
+            for idx in range(21):
                 try:
-                    master = mavutil.mavlink_connection(port)
-                    master.wait_heartbeat()
-                    print("SITL ile baglanti kuruldu.")
+                    master = mavutil.mavlink_connection(f'{port}{idx}')
+                    master.wait_heartbeat(blocking=False)
+                    print("Arac ile baglanti kuruldu.")
+                    break
                 except:
                     raise Exception(
-                        "SITL ile bağlanti kurulamadi port bilgisini kontrol edin.")
+                        "Arac ile baglanti kurulamadi."
+                        "Port bilgisinde hata var yada kablo duzgun takilmamis olabilir."
+                    )
+        elif baglanti_modu == "SITL":
+            port = 'udp:127.0.0.1:14550'
+            master = mavutil.mavlink_connection(port)
+            master.wait_heartbeat(timeout = 3)
+            if master.mav_count > 0:
+                # sitl ile baglandiysak heartbeat mesajını almış olmamız gerek
+                # heartbeat mesajı aldık isek 'mav_count' 0'dan buyuk olmalı
+                print("SITL ile baglanti kuruldu.")
             else:
                 raise Exception(
-                    "Araca USB ya da SITL ile baglanmadan hareket komutlarini calistiramazsiniz.")
-            return master
-        self.master = baglan()
-        pass
+                    "SITL ile bağlanti kurulamadi port bilgisini kontrol edin."
+                )
+        else:
+            raise Exception(
+                "Araca sadece USB ya da SITL ile baglanabilirsin."
+            )
 
-    def  hareket_et(self, x, y, z, r,t):
+        self.master = master
+
+        self.mode_map = self.master.mode_mapping()
+        self.mode_map_keys = tuple(self.mode_map.keys())
+        self.mode_map_values = tuple(self.mode_map.values())
+        return
+
+    def hareket_et(self, x, y, z, r, t = 1, i = 1):
         """
         x- ileri ve geri hareket,[-1000,1000] araligi(0 degeri hareket vermez)
         y- saga ve sola hareket,[-1000,1000] araligi(0 degeri hareket vermez)
         z- yukari ve asagi hareket,Uyarı! [0,1000] araligi(500 degeri hareket vermez)
         r- kendi etrafinda saga ve sola hareket,[-1000,1000] araligi(0 degeri hareket vermez)
-        t- hareketin kac saniye islecegi
+        t- komutun kac defa gonderilecegi
+        i- her komut arası beklenecek olan sure
         """
-        # t kac saniye hareketi yapacagi
-        while(True):
+        for _ in range(t):
             self.master.mav.manual_control_send(
                 self.master.target_system,
-                x,#ileri,geri
-                y,#sag gitme,sol gitme
-                z,#yukari asagi default 500 degeri
-                r,#etrafinda donme
+                x, #ileri,geri
+                y, #sag gitme,sol gitme
+                z, #yukari asagi default 500 degeri
+                r, #etrafinda donme
                 0  # Buton parametresi eger butona basılacaksa buton numarasina denk gelen bit degeri gonderilmeli
             )
-            time.sleep(1)
-            t=t-1
-            if(t==0):
-                break
-        return
+            sleep(i)
+        return self.SUCCESS
 
     def yunusbaligi(self):
-        #duzenlecek
-        return
+        ...
 
     def set_arm(self, arm : bool = True, max_try : int = 3):
         """
@@ -108,21 +111,16 @@ class Dolunay():
         Aracin modunu degistirmek icin kullanilir
         Ornek set_mod("DEPTH_HOLD") -> araci DEPTH_HOLD moda alir
         """
-        mode_map = self.master.mode_mapping()
-
         mode = mode.upper()
 
-        if mode not in mode_map:
-            print(\
-                f"{mode} modu bulunamadi."\
-                " ACRO moda gecilecek."\
+        if mode not in self.mode_map:
+            print(
+                f"{mode} modu bulunamadi."
+                " DEPTH_HOLD moda gecilecek."
             )
             mode = 'DEPTH_HOLD'
 
-        mode_id = mode_map[mode]
-
-        mode_map_keys = tuple(mode_map.keys())
-        mode_map_values = tuple(mode_map.values())
+        mode_id = self.mode_map[mode]
 
         for _ in range(max_try):
             self.master.mav.set_mode_send(
@@ -136,7 +134,7 @@ class Dolunay():
                 print('Couldnt receive heartbeat')
                 continue
 
-            current_mode = mode_map_keys[mode_map_values.index(hb.custom_mode)]
+            current_mode = self.mode_map_keys[self.mode_map_values.index(hb.custom_mode)]
 
             print("Mod -> ", current_mode)
 
