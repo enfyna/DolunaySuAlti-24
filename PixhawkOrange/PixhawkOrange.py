78a506a135b34c0946878ad61cd5bcaa19cea6a3
enfyna <legendraslan@gmail.com>
2023-10-11 07:39:03 +0300

    Optimize `get_mod()`. Added type hints to functions.

--- PixhawkOrange/PixhawkOrange.py
+++ PixhawkOrange/PixhawkOrange.py
@@ -49,9 +49,18 @@ class Dolunay():
         self.mode_map = self.master.mode_mapping()
         self.mode_map_keys = tuple(self.mode_map.keys())
         self.mode_map_values = tuple(self.mode_map.values())
+
+        hb = self.master.wait_heartbeat(blocking=True)
+
+        if hb.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED:
+            self.current_arm_state = 'ARM'
+        else:
+            self.current_arm_state = 'DISARM'
+
+        self.current_mode = self.mode_map_keys[self.mode_map_values.index(hb.custom_mode)]
         return
 
-    def hareket_et(self, x, y, z, r, t = 1, i = 1):
+    def hareket_et(self, x, y, z, r, t = 1, i = 1) -> int:
         """
         x- ileri ve geri hareket,[-1000,1000] araligi(0 degeri hareket vermez)
         y- saga ve sola hareket,[-1000,1000] araligi(0 degeri hareket vermez)
@@ -75,7 +84,7 @@ class Dolunay():
     def yunusbaligi(self):
         ...
 
-    def set_arm(self, arm : bool = True, max_try : int = 7):
+    def set_arm(self, arm : bool = True, max_try : int = 7) -> int:
         """
         (DISARM) arm = False icin arac kendini disarm eder ve komutlar calismaz
         (ARM) arm = True icin arac kendini arm eder ve komut almaya hazirdir
@@ -95,13 +104,15 @@ class Dolunay():
 
             if arm and self.master.motors_armed() == mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED:
                 print("-> Armed")
+                self.current_arm_state = 'ARM'
                 return self.SUCCESS
             elif not arm and self.master.motors_armed() == 0:
                 print("-> Disarmed")
+                self.current_arm_state = 'DISARM'
                 return self.SUCCESS
         return self.ERROR_OUT_OF_LOOP
 
-    def set_mod(self, mode : str = 'ALT_HOLD', max_try : int = 7):
+    def set_mod(self, mode : str = 'ALT_HOLD', max_try : int = 7) -> int:
         """
         Aracin modunu degistirmek icin kullanilir
         Ornek set_mod("ALT_HOLD") -> araci ALT_HOLD moda alir
@@ -129,135 +140,83 @@ class Dolunay():
                 print('Couldnt receive heartbeat')
                 continue
 
-            current_mode = self.mode_map_keys[self.mode_map_values.index(hb.custom_mode)]
+            self.current_mode = self.mode_map_keys[self.mode_map_values.index(hb.custom_mode)]
 
-            print("Mod -> ", current_mode)
+            print("Mod -> ", self.current_mode)
 
-            if mode == current_mode:
+            if mode == self.current_mode:
                 return self.SUCCESS
         return self.ERROR_OUT_OF_LOOP
-    
-    def getData(self):
-         lines = {}
-         lines.update(self.get_attitude())
-         lines.update(self.get_pressure())
-         lines.update(self.motors())
-         lines.update(self.get_mod())
-         return lines
 
-    def get_attitude(self):
-        self.master.mav.command_long_send(self.master.target_system,self.master.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
-                                         0,
-                                         30,0,0,0,0,0,0)
+    def getData(self) -> dict:
+        lines = {
+            **self.get_attitude(),
+            **self.get_pressure(),
+            **self.motors(),
+            **self.get_mod()
+        }
+        return lines
+
+    def get_attitude(self) -> dict:
+        self.master.mav.command_long_send(
+            self.master.target_system, self.master.target_component,
+            mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+            0,
+            30,0,0,0,0,0,0)
         attitude = self.master.recv_match(type='ATTITUDE', blocking=True)
-        yaw = attitude.yaw
-        roll = attitude.roll
-        pitch = attitude.pitch
         data = {
-            "yaw": yaw,
-            "roll": roll,
-            "pitch": pitch}
+            "yaw": attitude.yaw,
+            "roll": attitude.roll,
+            "pitch": attitude.pitch
+        }
         return data
 
-    def get_pressure(self):
-        self.master.mav.command_long_send(self.master.target_system,self.master.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,
-                                         29,0,0,0,0,0,0)
+    def get_pressure(self) -> dict:
+        self.master.mav.command_long_send(
+            self.master.target_system,self.master.target_component,
+            mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0,
+            29,0,0,0,0,0,0)
         pressure = self.master.recv_match(type='SCALED_PRESSURE', blocking=True)
-        pressure_abs = pressure.press_abs#hPa
-        pressure_diff = pressure.press_diff#hPa
+        pressure_abs = pressure.press_abs #hPa
+        pressure_diff = pressure.press_diff #hPa
         temperature = pressure.temperature
         #Depth (meters) = (Differential Pressure (hPa)) / (Density (kg/m³) * Gravity (m/s²) * 100)
         #In this formula, 100 is used to convert hPa to Pa because 1 hPa = 100 Pa.
         p = 1000.0
         g = 9.83
-        depth=pressure_diff/(p*g*100)
-        data =  { "pressure": depth
-                 }
+        depth = pressure_diff/(p*g*100)
+        data = {
+            "pressure": depth
+        }
         return data
 
-    def motors(self):
-        self.master.mav.command_long_send(self.master.target_system,self.master.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
-                                         0,
-                                         36,0,0,0,0,0,0)
+    def motors(self) -> dict:
+        self.master.mav.command_long_send(
+            self.master.target_system,self.master.target_component,
+            mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+            0,
+            36,0,0,0,0,0,0)
         engine = self.master.recv_match(type='SERVO_OUTPUT_RAW', blocking=True)
         # The SERVO_OUTPUT_RAW message contains data for 8 servo/engine outputs
         # You can access the data for each output using the following indices:
         # engine.servo1_raw, engine.servo2_raw, engine.servo3_raw, engine.servo4_raw
         # engine.servo5_raw, engine.servo6_raw, engine.servo7_raw, engine.servo8_raw
-        data ={"servo1":engine.servo1_raw,"servo2":engine.servo2_raw,
-               "servo3":engine.servo3_raw,"servo4":engine.servo4_raw,
-               "servo5":engine.servo5_raw,"servo6":engine.servo6_raw,
-               "servo7":engine.servo7_raw,"servo8":engine.servo8_raw}
+        data = {
+            "servo1":engine.servo1_raw,"servo2":engine.servo2_raw,
+            "servo3":engine.servo3_raw,"servo4":engine.servo4_raw,
+            "servo5":engine.servo5_raw,"servo6":engine.servo6_raw,
+            "servo7":engine.servo7_raw,"servo8":engine.servo8_raw
+        }
         return data
 
-    def decode_mode(base_mode, custom_mode):
-        """Decode flight mode from base_mode and custom_mode fields of HEARTBEAT message"""
-        mode = 'UNKNOWN'
-        if base_mode & mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED:
-            if custom_mode == 0:
-                mode = 'STABILIZE'
-            elif custom_mode == 1:
-                mode = 'ACRO'
-            elif custom_mode == 2:
-                mode = 'ALT_HOLD'
-            elif custom_mode == 3:
-                mode = 'AUTO'
-            elif custom_mode == 4:
-                mode = 'GUIDED'
-            elif custom_mode == 5:
-                mode = 'LOITER'
-            elif custom_mode == 6:
-                mode = 'RTL'
-            elif custom_mode == 7:
-                mode = 'CIRCLE'
-            elif custom_mode == 8:
-                mode = 'POSITION'
-            elif custom_mode == 9:
-                mode = 'LAND'
-            elif custom_mode == 10:
-                mode = 'OF_LOITER'
-            elif custom_mode == 11:
-                mode = 'DRIFT'
-            elif custom_mode == 13:
-                mode = 'SPORT'
-            elif custom_mode == 14:
-                mode = 'FLIP'
-            elif custom_mode == 15:
-                mode = 'AUTOTUNE'
-            elif custom_mode == 16:
-                mode = 'POSHOLD'
-            elif custom_mode == 17:
-                mode = 'BRAKE'
-            elif custom_mode == 18:
-                mode = 'THROW'
-            elif custom_mode == 19:
-                mode = 'MANUAL'
-            elif custom_mode == 20:
-                mode = 'GUIDED_NOGPS'
-        return mode
-
-    def get_mod(self):
-        self.master.mav.command_long_send(self.master.target_system,self.master.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
-                                         0,
-                                         0,0,0,0,0,0,0)
-        mode = self.master.recv_match(type='HEARTBEAT', blocking=True)
-        mode_name = self.decode_mode(mode.base_mode, mode.custom_mode)
-        armed = mode.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
-        armedtext = ""
-        if armed ==128:
-            armedtext = "ARMED"
-        else:
-            armedtext = "DISARM"
-        data={"mode":mode_name,
-              "arm":armedtext}
+    def get_mod(self) -> dict:
+        data = {
+            "mode": self.current_mode,
+            "arm": self.current_arm_state
+        }
         return data
-            
 
-    def kapat(self):
+    def kapat(self) -> None:
         """
         Pixhawk ile olan baglantiyi kapatir.
         """
