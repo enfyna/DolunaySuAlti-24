19bdae62b0ab2c9f405fb49a28efe4e065e719d0
enfyna <legendraslan@gmail.com>
2023-11-02 07:15:28 +0300

    Update M1.

--- MissionOne/M1.py
+++ MissionOne/M1.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from numpy import array
+from numpy import array, ones, uint8
 import cv2
 
 class State(Enum):
@@ -7,6 +7,7 @@ class State(Enum):
 	IDENTIFY_TARGET = 1
 	MOVE_TO_TARGET = 2
 	LAND_ON_TARGET = 3
+	NORMALIZE_POSITION = 4
 
 class M1():
 	current_state = State.SEARCH_RED_TARGET
@@ -15,56 +16,89 @@ class M1():
 	upper_cyan = array([100, 255, 255], int)
 
 	found_targets : list[float] = []
+	went_forward : int = 0
+	check_vertex : int = 0
 
-	def FindRed(self, front_img, bottom_img) -> tuple[int, int, int, int] | None:
+	def FindRed(self, front_img, bottom_img, left_distance) -> tuple[int, int, int, int] | None:
 		cv2.waitKey(24)
 
 		cv2.imshow("front", front_img)
 		cv2.imshow("bottom", bottom_img)
 
 		match self.current_state:
+			case State.NORMALIZE_POSITION:
+				if self.went_forward > 0:
+					self.went_forward -= 1
+					return -1000, 0, 500, 0
+				self.current_state = State.SEARCH_RED_TARGET
+
+
 			case State.SEARCH_RED_TARGET:
+				hx = front_img.shape[1] // 2
+				qx = hx // 2
+				front_img = front_img[:, hx - qx: hx + qx]
+				
 				frame = self.FindRedFromImage(front_img)
 				cv2.imshow('searching', frame)
-				res = self.GetContours(frame)
-				res = self.FindLargestContour(res)
-				if res is None:
-					return None
-				self.current_state = State.IDENTIFY_TARGET
-				return 0, 0, 500, 0
+								
+				frame = cv2.erode(frame, ones((5, 5), uint8))  
+				cv2.imshow('erode', frame)
+
+				for row in frame:
+					if any(row):
+						if len(self.found_targets) > 0 and self.found_targets[-1] > left_distance - 2:
+							return None
+						self.found_targets.append(left_distance)
+						self.current_state = State.IDENTIFY_TARGET
+						return 0, 0, 500, 0
+				return None
+
 
 			case State.IDENTIFY_TARGET:
 				frame = self.FindRedFromImage(front_img)
-				cv2.imshow('identify', frame)
+				cv2.imshow('center', frame)
+
 				res = self.GetContours(frame)
 				if res is None:
 					self.current_state = State.SEARCH_RED_TARGET
-					return 0, 0, 500, 0
-				else:
-					lc = self.FindLargestContour(res)
-					if lc is not None:
-						self.current_state = State.MOVE_TO_TARGET
+					return None
+				
+				for c in res:
+					M = cv2.moments(c)
+					if M['m00'] < 20:
+						self.went_forward += 1
+						return 1000, 0, 500, 0
+
+				for c in res:
+					if self.GetVertexCount(c) > 8:
+						self.check_vertex += 1
+						if self.check_vertex > 5:
+							self.check_vertex = 0
+							self.current_state = State.MOVE_TO_TARGET
 						return 0, 0, 500, 0
+
+				self.current_state = State.NORMALIZE_POSITION
+				return 0, 0, 500, 0
 
 
 			case State.MOVE_TO_TARGET:
 				frame = self.FindRedFromImage(front_img)
 				cv2.imshow('moving', frame)
 				res = self.GetContours(frame)
-				res = self.FindLargestContour(res, 8)
-				if res is not None:
-					cx = int(res['m10'] / res['m00'])
-					cy = int(res['m01'] / res['m00'])
+				M, c = self.FindLargestContour(res, 8)
+				if M is not None:
+					cx = int(M['m10'] / M['m00'])
+					cy = int(M['m01'] / M['m00'])
 					# Secilen c'nin momentinden merkezini hesapla
 					diff_center = int(front_img.shape[1] // 2 - cx) * 10
 					return max(0, int(1000 -(abs(diff_center) * 2))), 0, 500, diff_center
 
 				frame = self.FindRedFromImage(bottom_img)
 				res = self.GetContours(frame)
-				res = self.FindLargestContour(res, 8)
-				if res is not None:
-					cx = int(res['m10'] / res['m00'])
-					cy = int(res['m01'] / res['m00'])
+				M, c = self.FindLargestContour(res, 8)
+				if M is not None:
+					cx = int(M['m10'] / M['m00'])
+					cy = int(M['m01'] / M['m00'])
 					diff_x = int(front_img.shape[1] // 2 - cx)
 					diff_y = int(front_img.shape[0] // 2 - cy)
 					if abs(diff_x) > 50 or abs(diff_y) > 50:
@@ -77,10 +111,10 @@ class M1():
 				frame = self.FindRedFromImage(bottom_img)
 				cv2.imshow("landing", frame)
 				res = self.GetContours(frame)
-				res = self.FindLargestContour(res)
-				if res is not None:
-					cx = int(res['m10'] / res['m00'])
-					cy = int(res['m01'] / res['m00'])
+				M, c = self.FindLargestContour(res)
+				if M is not None:
+					cx = int(M['m10'] / M['m00'])
+					cy = int(M['m01'] / M['m00'])
 					diff_x = int(front_img.shape[1] // 2 - cx)
 					diff_y = int(front_img.shape[0] // 2 - cy)
 					z = 500
@@ -97,6 +131,14 @@ class M1():
 		return image
 
 	@staticmethod
+	def GetContourArea(contour) -> float:
+		return cv2.moments(contour)['m00']
+
+	@staticmethod
+	def GetCenterPoint(moment) -> tuple[int, int]:
+		return int(moment['m10'] / moment['m00']), int(moment['m01'] / moment['m00'])
+
+	@staticmethod
 	def GetVertexCount(contour) -> int:
 		return len(cv2.approxPolyDP(
 			contour, 0.01 * cv2.arcLength(contour, True), True
@@ -109,8 +151,8 @@ class M1():
 		)[0]
 
 	@staticmethod
-	def FindLargestContour(contours, min_vertex_count = 6) -> cv2.typing.Moments | None:
-		anlik_max_c_alani, scm = 0.0, None
+	def FindLargestContour(contours, min_vertex_count = 6) -> tuple[cv2.typing.Moments, cv2.UMat]:
+		current_max_area, moment, contour = 0.0, None, None
 
 		for c in contours:
 			approx = cv2.approxPolyDP(
@@ -121,9 +163,9 @@ class M1():
 
 			M = cv2.moments(c)
 
-			if M['m00'] > anlik_max_c_alani:
-				scm , anlik_max_c_alani = M , M['m00']
+			if M['m00'] > current_max_area:
+				moment, contour, current_max_area = M, c, M['m00']
 				# Bulunan en buyuk c'yi anlik olarak kaydet
-				# Not : scm -> secilen c momenti
+				# Not : moment -> secilen c momenti
 
-		return scm
+		return moment, contour
--- MissionOne/main.py
+++ MissionOne/main.py
@@ -14,17 +14,25 @@ from sys import path, argv
 # pythona o klasoru bulabilecegi dosya yolunu gostermemiz lazim.
 # Komut satırında 'cd ..' komutunu kullanıyormus gibi dusunulebilinir.
 
-# DolunaySuAlti-24 klasorunu bul
-ROOT_PATH = '/'.join(abspath(__file__).split('/')[0:-2]) # linux
-
-# path'a ekle
-path.append(ROOT_PATH)
-
 # Dolunayı simdi import edebiliriz
 if len(argv) == 1:
-	from PixhawkOrange.PixhawkOrange import Dolunay
+
+	# DolunaySuAlti-24/PixhawkOrange klasorunu bul
+	PO_PATH = '/'.join(abspath(__file__).split('/')[0:-2]) + '/PixhawkOrange' # linux
+
+	# path'a ekle
+	path.append(PO_PATH)
+	
+	from Dolunay import Dolunay
 else:
-	from DMS.DolunaySim import Dolunay
+
+	# DolunaySuAlti-24/DMS klasorunu bul
+	DMS_PATH = '/'.join(abspath(__file__).split('/')[0:-2]) + '/DMS' # linux
+
+	# path'a ekle
+	path.append(DMS_PATH)
+
+	from DolunaySim import Dolunay
 
 arac = Dolunay()
 
@@ -34,32 +42,31 @@ mission = M1()
 
 # Gorev Kodu
 
-arac.set_arm(True)
+arac.Pixhawk.set_arm(True)
 
-arac.set_mod('ACRO')
+arac.Pixhawk.set_mod('ACRO')
 try:
 	while True:
 		# Gorev algoritmasını burada calıstıracagız
-
-		is_front_cam, frame_front = arac.get_front_cam()
-		is_bottom_cam, frame_bottom = arac.get_bottom_cam()
+		is_front_cam, frame_front = arac.Camera.get_front_cam()
+		is_bottom_cam, frame_bottom = arac.Camera.get_bottom_cam()
 
 		if not is_front_cam or not is_bottom_cam:
 			continue
 
-		move = mission.FindRed(frame_front, frame_bottom)
-		print(f'{State(mission.current_state)}')
+		move = mission.FindRed(frame_front, frame_bottom, arac.Distance.getLeftDistance()[0])
+		# print(f'{State(mission.current_state)}')
 
 		if move is not None:
 			# print(f"mission move -> {move}")
-			arac.hareket_et(*move, 1, 0)
+			arac.Pixhawk.hareket_et(*move, 1, 0)
 		else:
 			# print("search  move -> (0, -1000, 500, 0)")
-			arac.hareket_et(0, -1000, 500, 0, 1, 0)
+			arac.Pixhawk.hareket_et(0, -1000, 500, 0, 1, 0)
 except Exception as e:
-	print(f'Exception: {e}')
+	print(f'Hata: {e}')
 
-arac.set_arm(False)
+arac.Pixhawk.set_arm(False)
 
-arac.release_cams()
+arac.Camera.release_cams()
 # connStation.close()
