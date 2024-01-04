c38019e0df0d21d8f62d0559df46309f1b9fb1ce
enfyna <legendraslan@gmail.com>
2024-01-04 14:14:37 +0300

    Add `set_target_depth()` to `PixhawkOrange`.

--- PixhawkOrange/PixhawkOrange.py
+++ PixhawkOrange/PixhawkOrange.py
@@ -1,5 +1,5 @@
 from pymavlink import mavutil
-from time import sleep
+from time import sleep, time
 
 class PixhawkOrange():
 
@@ -58,6 +58,8 @@ class PixhawkOrange():
             self.current_arm_state = 'DISARM'
 
         self.current_mode = self.mode_map_keys[self.mode_map_values.index(hb.custom_mode)]
+
+        self.boot_time = time()
         return
 
     def hareket_et(self, x, y, z, r, t = 1.0, i = 0) -> int:
@@ -146,6 +148,40 @@ class PixhawkOrange():
             if mode == self.current_mode:
                 return self.SUCCESS
         return self.ERROR_OUT_OF_LOOP
+
+    def set_target_depth(self, target : float) -> int:
+        """ Sets the target depth while in depth-hold mode.
+
+        Uses https://mavlink.io/en/messages/common.html#SET_POSITION_TARGET_GLOBAL_INT
+
+        'depth' is technically an altitude, so set as negative meters below the surface
+            -> set_target_depth(-1.5) # sets target to 1.5m below the water surface.
+
+        """
+        master.mav.set_position_target_global_int_send(
+            int(1e3 * (time() - self.boot_time)), # ms since boot
+            master.target_system, master.target_component,
+            coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_INT,
+            type_mask=( # ignore everything except z position
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_X_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_Y_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_Z_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VZ_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_FORCE_SET |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE
+            ), lat_int=0, lon_int=0, alt=depth, # (x, y WGS84 frame pos - not used), z [m]
+            vx=0, vy=0, vz=0, # velocities in NED frame [m/s] (not used)
+            afx=0, afy=0, afz=0, yaw=0, yaw_rate=0
+            # accelerations in NED frame [N], yaw, yaw_rate
+            #  (all not supported yet, ignored in GCS Mavlink)
+        )
+        return self.SUCCESS
 
     def getData(self) -> dict:
         lines = {}
