08ec95a8a5298e1653c64e2c5b2fb62afe59cc39
enfyna <legendraslan@gmail.com>
2024-03-30 14:25:22 +0300

    Fix variable name.

--- PixhawkOrange/PixhawkOrange.py
+++ PixhawkOrange/PixhawkOrange.py
@@ -160,9 +160,9 @@ class PixhawkOrange():
             -> set_target_depth(-1.5) # sets target to 1.5m below the water surface.
 
         """
-        master.mav.set_position_target_global_int_send(
+        self.master.mav.set_position_target_global_int_send(
             int(1e3 * (time() - self.boot_time)), # ms since boot
-            master.target_system, master.target_component,
+            self.master.target_system, self.master.target_component,
             coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_INT,
             type_mask=( # ignore everything except z position
                 mavutil.mavlink.POSITION_TARGET_TYPEMASK_X_IGNORE |
@@ -177,7 +177,7 @@ class PixhawkOrange():
                 # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_FORCE_SET |
                 mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_IGNORE |
                 mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE
-            ), lat_int=0, lon_int=0, alt=depth, # (x, y WGS84 frame pos - not used), z [m]
+            ), lat_int=0, lon_int=0, alt=target, # (x, y WGS84 frame pos - not used), z [m]
             vx=0, vy=0, vz=0, # velocities in NED frame [m/s] (not used)
             afx=0, afy=0, afz=0, yaw=0, yaw_rate=0
             # accelerations in NED frame [N], yaw, yaw_rate
